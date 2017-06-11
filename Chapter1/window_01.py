626d96e7c9f58db94e4a0510afbf79b32e51a1d4
Suslanger <ilyabelick@gmail.com>
2017-06-11 10:47:09 +0300

    Create window_01.py

---
+++ Chapter1/window_01.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# импортирование модулей python
+from tkinter import *
+
+#создание окна
+root = Tk()
+root.title('myWindow')
+root.geometry('200x150+300+225')
+
+# вывод окна на экран
+root.mainloop()
