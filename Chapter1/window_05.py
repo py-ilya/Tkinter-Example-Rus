1dc1470502573d0ef49f96ee22e7c3b4675e4e4d
Suslanger <ilyabelick@gmail.com>
2017-06-11 18:12:01 +0300

    Create window_05.py

---
+++ Chapter1/window_05.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# импортирование модулей python
+from tkinter import *
+
+# класс главного окна
+class main:
+  def __init__(self, master):
+    self.master = master
+    self.master.title('parent')
+    self.master.geometry('200x150+300+225')
+    self.button = Button(self.master,
+                         text = 'myButton',
+                         command = self.openDialog)
+    self.button.pack(side = BOTTOM)
+    self.master.mainloop()
+
+  def openDialog(self):
+    child(self.master)
+
+# класс дочерних окон
+class child:
+  def __init__(self, master):
+    self.slave = Toplevel(master)
+    self.slave.title('child')
+    self.slave.geometry('200x150+500+375')
+
+# создание окна
+root = Tk()
+
+# запуск окна
+main(root)
